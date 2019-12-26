5eb64243ce43bee842ec5483da038a2d843d9c7c
James Priest <james-priest@outlook.com>
2019-12-26 13:59:57 -0800

    feat: Add LinkedList sort

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { Sorter } from './Sorter';
 import { NumbersCollection } from './NumbersCollection';
 import { CharactersCollection } from './CharactersCollection';
+import { LinkedList } from './LinkedList';
 
 const numbersCollection = new NumbersCollection([12, 3, -5, 0, -10, 2]);
 const numSorter = new Sorter(numbersCollection);
@@ -13,3 +14,16 @@ const strSorter = new Sorter(charactersCollection);
 console.log('pre sort:', charactersCollection.data);
 strSorter.sort();
 console.log('post sort:', charactersCollection.data);
+
+const linkedList = new LinkedList();
+linkedList.add(500);
+linkedList.add(-10);
+linkedList.add(-3);
+linkedList.add(4);
+
+const llSorter = new Sorter(linkedList);
+console.log('-- pre sort --');
+linkedList.print();
+llSorter.sort();
+console.log('-- post sort --');
+linkedList.print();
